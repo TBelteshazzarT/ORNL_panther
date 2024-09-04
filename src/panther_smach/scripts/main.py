584d41e566210556196da272740beff4d1e297d2
ORNL-ROV <belteshazzarboyd@gmail.com>
2024-09-04 09:44:44 -0400

    updated trav_sm with goal targets for move base

--- src/panther_smach/scripts/main.py
+++ src/panther_smach/scripts/main.py
@@ -9,6 +9,29 @@ from trav_sm import Trav_sm
 #main
 def main():
     rospy.init_node('smach_plan')
+    cask_x = 4
+    cask_y = 2
+    x = -10.9728
+    y = -5
+    i=0
+    j=0
+    k =0
+    temp = -1
+    theta = 0.785398
+    location_array = []
+    
+    while i <= cask_x:
+        temp = -temp
+        location_array.append([x, y, theta])
+        k += 1
+        while j < cask_y:
+            y = y + (5*temp)
+            location_array.append([x, y, theta])
+            j = j + 1
+            k+= 1
+        j = 0
+        x = x + 5.4864
+        i = i+1
 
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['undocking_error', 'travel_error', 'docking_error', 'loop_complete'])
@@ -17,7 +40,7 @@ def main():
     with sm:
         # Add states to the container (what outcome moves to what state)
         smach.StateMachine.add('Undock', Undock(), transitions={'complete':'Trav_sm', 'error':'undocking_error'})
-        smach.StateMachine.add('Trav_sm', Trav_sm(), transitions={'complete':'Dock', 'error':'travel_error'})
+        smach.StateMachine.add('Trav_sm', Trav_sm(location_array), transitions={'complete':'Dock', 'error':'travel_error'})
         smach.StateMachine.add('Dock', Dock(), transitions={'complete':'loop_complete', 'error':'docking_error'})
 
     # Execute SMACH plan
--- src/panther_smach/scripts/trav_sm.py
+++ src/panther_smach/scripts/trav_sm.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
-import rospy
 import smach
+import smach_ros
+import rospy
+import tf.transformations as tft
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import actionlib
+
+goal = MoveBaseGoal()
 
 # Mainstate
 
 # define state Trav_sm
 class Trav_sm(smach.State):
-    def __init__(self):
+    def __init__(self, location_array):
         smach.State.__init__(self, outcomes=['complete','error'])
+        self.location_array = location_array
+        self.move_base_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        self.move_base_client.wait_for_server()
     
     def execute(self, userdata):
         rospy.loginfo('Executing state Trav_sm')
@@ -18,8 +27,10 @@ class Trav_sm(smach.State):
 
         # Open with the container
         with sm:
-            # Add substates to the container
-            smach.StateMachine.add('main_nav', main_nav(), transitions={'sub_complete':'complete', 'sub_error':'manual_override'})
+            for location in range(len(self.location_array)):
+            
+                # Add substates to the container
+                smach.StateMachine.add(f'main_drive_{location}', main_nav(self.location_array[location], self.move_base_client), transitions={'succeeded':'complete', 'aborted':'manual_override'})
             smach.StateMachine.add('manual_override', manual_override(), transitions={'sub_complete':'complete'})
 
         # Sub-execute SMACH plan
@@ -32,14 +43,36 @@ class Trav_sm(smach.State):
 
 # define substate main_nav    
 class main_nav(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['sub_complete', 'sub_error'])
+    def __init__(self, location, move_base_client):
+        smach.State.__init__(self, outcomes=['succeeded', 'aborted'])
+        self.x = location[0]
+        self.y = location[1]
+        self.theta = location[2]
+        self.move_base_client = move_base_client
 
     def execute(self, userdata):
         rospy.loginfo('Executing main_nav substate')
-        return 'sub_error'
+        goal.target_pose.header.frame_id = "map"
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = self.x
+        goal.target_pose.pose.position.y = self.y
+
+        
+        quaternion = tft.quaternion_from_euler(0, 0, self.theta)
+    
+        goal.target_pose.pose.orientation.x = quaternion[0]
+        goal.target_pose.pose.orientation.y = quaternion[1]
+        goal.target_pose.pose.orientation.z = quaternion[2]
+        goal.target_pose.pose.orientation.w = quaternion[3]
+
+        self.move_base_client.wait_for_server()
+        self.move_base_client.send_goal(goal)
+
+        state = self.move_base_client.get_state()
+        return state
 
 # define substate manual_override
+#daniel
 class manual_override(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['sub_complete'])
@@ -47,3 +80,6 @@ class manual_override(smach.State):
     def execute(self, userdata):
         rospy.loginfo('Executing manual_override substate')
         return 'sub_complete'
+    
+
+
