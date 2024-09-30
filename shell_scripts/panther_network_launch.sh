7354f38bd5d29a2d5e599b27c932bca745aef0ce
ORNL-ROV <belteshazzarboyd@gmail.com>
2024-09-30 14:30:33 -0400

    new shell scripts

---
+++ shell_scripts/panther_network_launch.sh
@@ -0,0 +1,4 @@
+echo launching main Panther ...
+export ROS_MASTER_URI=http://192.168.5.235:11311
+export ROS_IP=192.168.5.235
+roslaunch sdr_launch main.launch
---
+++ shell_scripts/panther_rviz.sh
@@ -0,0 +1,4 @@
+echo opening rviz...
+export ROS_MASTER_URI=http://192.168.5.235.11311
+rosrun rviz rviz -d panther_visual/config/rviz_default.rviz
+
