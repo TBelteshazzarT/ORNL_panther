007633b2a181afcb8c1cf3ac42a9dffefafd7eb6
Daniel <belteshazzarboyd@gmail.com>
2024-10-04 14:57:55 -0400

    Added cylinder_detection.py updates

--- src/panther_smach/scripts/cylinder_detection.py
+++ src/panther_smach/scripts/cylinder_detection.py
@@ -1,39 +1,55 @@
 #!/usr/bin/env python3
 
 import rospy
-from sensor_msgs.msg import LaserScan
+#from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import OccupancyGrid
 import sensor_msgs.msg
 import tf
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import os
 
+def home():
+    return os.path.expanduser("~")
 
+def grid_callback(data):
+    obstical_coords = []
+    print(data.info.width)
+    print(data.info.height)
+    for width in range(data.info.width - 1):
+        for height in range(data.info.height - 1):
+            if data.data[height * data.info.width + width] == 100:
+                x = width * data.info.resolution + data.info.resolution / 2
+                y = height * data.info.resolution + data.info.resolution / 2
+                obstical_coords.append([x,y])
+    #write(obstical_coords)
+   
 def main():
-    listener()
-
-def listener():
-    rospy.init_node('small_scan_node')
-    scan_sub = rospy.Subscriber('/scan', LaserScan, callback)
+    rospy.init_node('grid_test')
+    grid_sub = rospy.Subscriber('/map', OccupancyGrid, grid_callback)
     rospy.spin()
 
-def tf_listener():
-    pose_listener = tf.TransformListener()
-    pose_listener.waitForTransform('/base_footprint', '/map', rospy.Time(), rospy.Duration(4.0))
-    try:
-        
-        (trans, rot) = pose_listener.lookupTransform('/base_footprint', '/map', rospy.Time(0))
-        print('Translation', trans)
-        print('Rotation', rot)
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        pass
+def write(data):
+    with open(str(home()) + '/grid.csv', 'w') as f:
+        csvwriter = csv.writer(f)
+        for k in data:
+            line = [k[0], k[1]]
+            csvwriter.writerow(line)
+            
+
+def plot(data):
+    x = np.array([])
+    y = np.array([])
+    for k in data:
+        x = np.append(x, [k[0]])
+        y = np.append(y, [k[1]])
+    plt.scatter(x,y)
+
+
     
 
-def callback(msg):
-    #print(len(msg.ranges))
-    smallest_value = msg.ranges[0]
-    for i in msg.ranges:
-        if i < smallest_value:
-            smallest_value = i
-    print('The smallest value is', smallest_value)
-    tf_listener()
+
 
 if __name__ == '__main__':
     main()
