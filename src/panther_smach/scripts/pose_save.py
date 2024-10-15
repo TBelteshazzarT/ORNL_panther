d9370f912f9214eb6478b090e1d54db6b46d6c50
Daniel <belteshazzarboyd@gmail.com>
2024-10-15 14:03:38 -0400

    Added pose_save.py to record route

--- src/panther_smach/scripts/pose_save.py
+++ src/panther_smach/scripts/pose_save.py
@@ -7,24 +7,43 @@ import sensor_msgs.msg
 import tf
 import csv
 import os
+from panther_smach.srv import PoseSave
 
 saved_coords = []
+save_bool = False
 
 def home():
     return os.path.expanduser("~")
 
 def pose_callback(data):
-    global saved_coords
-    x = data.position.x
-    y = data.position.y
-    saved_coords.append((x,y))
-    print(saved_coords)
+    global saved_coords, save_bool
+    if save_bool:
+    	x = data.position.x
+    	y = data.position.y
+    	theta = data.orientation.z
+    	saved_coords.append([x,y,theta])
+    save_bool = False
 
+def handle_save_pose(req):
+    global save_bool
+    save_bool = True
+    return req
+    
+def shutdownhook():
+    rospy.loginfo("Saving Route to csv..")
+    global saved_coords
+    with open(str(home()) + '/panther_ws/src/panther_smach/saved_route/route.csv', 'w') as f:
+        csvwriter = csv.writer(f)
+        for k in saved_coords:
+            line = [k[0], k[1], k[2]]
+            csvwriter.writerow(line)
    
 def main():
     rospy.init_node('pose_save')
     pose_sub = rospy.Subscriber('/robot_pose', Pose, pose_callback)
+    s = rospy.Service('pose_save', PoseSave, handle_save_pose)
     rospy.spin()
+    rospy.on_shutdown(shutdownhook)
 
 
     
